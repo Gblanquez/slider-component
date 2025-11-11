621f5de3be816521865388e87b65cd9a1e466ae5
Gblanquez <hellogabrielblanquez@gmail.com>
2025-11-11 18:05:42 +0100

    change to load

--- src/main.js
+++ src/main.js
@@ -3,11 +3,15 @@ import sliderInit from './slider'
 import underlineAnimation from './link'
 
 document.addEventListener("DOMContentLoaded", () => {
-  const elements = document.querySelectorAll('[data-a="underline"]');
-  if (elements.length > 0) {
-    underlineAnimation();
-  }
-  sliderInit();
-});
+    const elements = document.querySelectorAll('[data-a="underline"]');
+    if (elements.length > 0) {
+
+      requestAnimationFrame(() => {
+        underlineAnimation();
+      });
+    }
+    sliderInit();
+  });
+  
 
 
